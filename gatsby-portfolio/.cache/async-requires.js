1b7aca83ef415d931c3d4c44d225dab2291262e8
JKHackathon <penguinkim21@gmail.com>
2024-05-05 19:26:50 -0400

    Reverted repo to store only project reports

---
+++ gatsby-portfolio/.cache/async-requires.js
@@ -0,0 +1,11 @@
+exports.components = {
+  "component---cache-dev-404-page-js": () => import("./../../dev-404-page.js?export=default" /* webpackChunkName: "component---cache-dev-404-page-js" */),
+  "component---src-templates-using-dsg-js": () => import("./../../../src/templates/using-dsg.js?export=default" /* webpackChunkName: "component---src-templates-using-dsg-js" */)
+}
+
+
+
+exports.head = {
+  "component---src-templates-using-dsg-js": () => import("./../../../src/templates/using-dsg.js?export=head" /* webpackChunkName: "component---src-templates-using-dsg-jshead" */)
+}
+
---
+++ gatsby-portfolio/.cache/sync-requires.js
@@ -0,0 +1,10 @@
+
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": preferDefault(require("/Users/joelscomputer/Desktop/cs1300/cs1300-portfolio/gatsby-portfolio/.cache/dev-404-page.js")),
+  "component---src-templates-using-dsg-js": preferDefault(require("/Users/joelscomputer/Desktop/cs1300/cs1300-portfolio/gatsby-portfolio/src/templates/using-dsg.js"))
+}
+
